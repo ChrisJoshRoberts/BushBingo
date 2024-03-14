08dc463479f5ccb61db34ee1d8475f548cac0796
Geraldine Cassie <cassiemakaza447@gmail.com>
2024-03-14 16:26:43 +0200

    Added a button and some links to other pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
 
-        has_many :game_players
-        has_many :games, through: :game_players
+  has_many :game_players
+  has_many :games, through: :game_players
 end
