4ccc57767310ddfbb969790581901ffa37a5d160
Lynn Musabayane <musabayanelynn2@gmail.com>
2024-03-15 14:03:02 +0200

    Add dotenv-rails

--- app/models/park_animal.rb
+++ app/models/park_animal.rb
@@ -1,4 +1,25 @@
+# class ParkAnimal < ApplicationRecord
+#   include PgSearch::Model
+#   pg_search_scope :search_by_animal_name,
+#   associated_against: {
+#     animal: [:name]
+#   },
+#   using: {
+#     tsearch: { prefix: true }
+#   }
+#   belongs_to :animal
+#   belongs_to :park
+# end
+
 class ParkAnimal < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_animal_name,
+                  associated_against: {
+                    animal: [:name]
+                  },
+                  using: {
+                    tsearch: { prefix: true }
+                  }
   belongs_to :animal
   belongs_to :park
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.1].define(version: 2024_03_14_154432) do
-=======
-
-ActiveRecord::Schema[7.1].define(version: 2024_03_14_123525) do
->>>>>>> parent of 32600c7 (starting styling)
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
