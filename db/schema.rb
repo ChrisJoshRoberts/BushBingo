1148a3d5a30001ed22863595b1e786d9a97d4586
Chris Roberts <chrisrobertscollective@gmail.com>
2024-03-15 08:57:32 +0200

    errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.1].define(version: 2024_03_14_123525) do
-=======
 ActiveRecord::Schema[7.1].define(version: 2024_03_14_154432) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
