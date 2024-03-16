c0a21616eba65a6abbe10b251c3ef67cb3d93ca3
Chris Roberts <chrisrobertscollective@gmail.com>
2024-03-16 12:26:32 +0200

    redesigning laning page and rules page

--- app/javascript/controllers/landing_page_controller.js
+++ app/javascript/controllers/landing_page_controller.js
@@ -5,10 +5,11 @@ export default class extends Controller {
   static targets = ["card"]
   connect() {
     console.log(this.cardTarget)
-    setTimeout(() => this.showCard(), 1500)
+    setTimeout(() => this.showCard(), 500)
   }
 
   showCard() {
     this.cardTarget.classList.remove("d-none");
+    this.cardTarget.classList.add("show")
   }
 }
