afc053df521a229f4e7970fa6eb57b7728a503d6
Chris Roberts <chrisrobertscollective@gmail.com>
2024-03-17 14:50:52 +0200

    staring on avatar page

--- app/javascript/controllers/landing_page_controller.js
+++ app/javascript/controllers/landing_page_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="landingpage"
 export default class extends Controller {
   static targets = ["card"]
+  
   connect() {
     console.log(this.cardTarget)
     setTimeout(() => this.showCard(), 500)
