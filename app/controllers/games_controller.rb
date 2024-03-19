8139592d6a28eaa921022a633ee96cb9396680c9
Lynn Musabayane <musabayanelynn2@gmail.com>
2024-03-19 12:43:06 +0200

    added park animals and users to seeds

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,10 +2,7 @@ class GamesController < ApplicationController
   before_action :set_game, only: %i[ show ]
 
   def index
-<<<<<<< HEAD
-=======
     @game = current_user.game_players.last.game
->>>>>>> master
     @accepted_games = current_user.game_players.joins(:game).where(game_players: { status: "accepted" }).select("games.*")
     @pending_games = current_user.game_players.joins(:game).where(game_players: { status: "pending" }).select("games.*")
   end
